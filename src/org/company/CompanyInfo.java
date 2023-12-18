49feb523e4b80503d61130fbabc3d60a4b97e4ad
prakashspsgitacc <prakashmnr1@gmail.com>
2023-12-18 12:48:27 +0530

    ronaldogit

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -1,30 +1,34 @@
 package org.company;
 
 public class CompanyInfo {
-	
+
 //	Methods     :companyName(),companyId(),companyAddress()
 
- private void companyName() {
-	 System.out.println("Amazon");
-	 
+	private void companyName() {
+		System.out.println("Amazon");
 
-}
- private void companyId() {
-	 System.out.println("168371");
+	}
 
-}
- private void companyAddress() {
-	 System.out.println("Bangalore");
-	 
-}
- public static void main(String[] args) {
-	 
-	CompanyInfo comp = new CompanyInfo();
-	comp.companyAddress();
-	comp.companyName();
-	comp.companyId();
-	
-	
-			
-}
+	private void companyId() {
+		System.out.println("168371");
+
+	}
+
+	private void companyAddress() {
+		System.out.println("Bangalore");
+
+	}
+
+	private void compmayMob() {
+		System.out.println("9774852368");
+	}
+
+	public static void main(String[] args) {
+
+		CompanyInfo comp = new CompanyInfo();
+		comp.companyAddress();
+		comp.companyName();
+		comp.companyId();
+
+	}
 }
